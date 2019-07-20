6a0a2e766d42b54284fa0bb3ce237e71f9b16807
Minsoo Kim <leios76@gmail.com>
2019-07-20 12:32:40 +0900

    기본 timeout 60초로 변경

--- create.sh
+++ create.sh
@@ -12,5 +12,5 @@ NAME=$(basename $(realpath .))
 zip -r ${NAME}-modules.zip nodejs/node_modules && \
     LAYER_VERSION=$(aws lambda publish-layer-version --layer-name ${NAME}-modules --zip-file fileb://${NAME}-modules.zip | jq -r .LayerVersionArn) && \
     cd nodejs && zip -r ../${NAME}.zip . -x node_modules\* && cd .. && \
-    aws lambda create-function --function-name ${NAME} --zip-file fileb://${NAME}.zip --handler index.handler --runtime nodejs10.x --role $1 --timeout 30 --layers ${LAYER_VERSION} --environment Variables="{}" && \
+    aws lambda create-function --function-name ${NAME} --zip-file fileb://${NAME}.zip --handler index.handler --runtime nodejs10.x --role $1 --timeout 60 --layers ${LAYER_VERSION} --environment Variables="{}" && \
     aws lambda create-alias --function-name ${NAME} --name service --function-version \$LATEST
